b64e6766a2a0a8d4e0651aa65aee9df7fc678ad3
Adaline <79500656+FlyVoid@users.noreply.github.com>
2021-12-19 23:16:22 +0800

    Add files via upload

---
+++ Class 04/Demo12a.java
@@ -0,0 +1,15 @@
+// Example for PPT page No.24
+
+public class Demo12a {
+	public static void main(String[] args) {
+		String[] s={"Beijing", "Shanghai", "Tianjin", "Chongqing"};
+		
+		System.out.println("This is class Demo12a");
+
+		System.out.println();
+		Demo12b.main(s);
+		System.out.println();
+		
+		System.out.println("This is also class Demo12a");
+	}
+}
---
+++ Class 04/Demo12b.java
@@ -0,0 +1,13 @@
+// Example for PPT page No.24
+
+public class Demo12b {
+	public static void main(String[] a) {
+		System.out.println("This is class Demo12b");
+		
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		
+		System.out.println("Demo12b is finished!");
+	}
+}
